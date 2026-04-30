4973912cd6a0bfc362eea62ed4dbb9036d9a9039
AveryG-883 <averyg883@icloud.com>
2026-04-30 09:09:24 -0400

    Update at 'Thu Apr 30 09:09:24 EDT 2026'

--- src/main/java/frc/robot/subsystems/launcher/LookupTables.java
+++ src/main/java/frc/robot/subsystems/launcher/LookupTables.java
@@ -6,19 +6,19 @@ import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 
 public class LookupTables {
     public static final InterpolatingDoubleTreeMap flywheelRpmHub = InterpolatingDoubleTreeMap.ofEntries(
-        Map.entry(1.09, 2005.0),
-        Map.entry(1.50, 2005.0),
-        Map.entry(2.00, 2005.0),
-        Map.entry(2.50, 1950.0),
-        Map.entry(3.00, 2020.0),
-        Map.entry(3.50, 2080.0),
-        Map.entry(4.00, 2190.0),
-        Map.entry(4.50, 2325.0),
-        Map.entry(5.00, 2460.0),
-        Map.entry(5.50, 2555.0),
-        Map.entry(6.00, 2680.0),
-        Map.entry(7.00, 2975.0),
-        Map.entry(8.00, 3245.0)
+        Map.entry(1.09, 1965.0),
+        Map.entry(1.50, 1965.0),
+        Map.entry(2.00, 1965.0),
+        Map.entry(2.50, 1910.0),
+        Map.entry(3.00, 1980.0),
+        Map.entry(3.50, 2040.0),
+        Map.entry(4.00, 2150.0),
+        Map.entry(4.50, 2285.0),
+        Map.entry(5.00, 2420.0),
+        Map.entry(5.50, 2515.0),
+        Map.entry(6.00, 2640.0),
+        Map.entry(7.00, 2935.0),
+        Map.entry(8.00, 3205.0)
     );
 
     public static final InterpolatingDoubleTreeMap hoodAngleHub = InterpolatingDoubleTreeMap.ofEntries(
